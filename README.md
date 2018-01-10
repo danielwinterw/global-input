16ee6feeb3e8d2cbba95b9e59bcc0e42f2d120cd
Dan Winter-Wijntjes <danwinter1@me.com>
2018-01-11 10:45:39 +1100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # global-input
+A browser library for detecting input without an input.
+
+## Installation
+```
+npm install --save global-input
+```
+
+## Usage
+### Standard Behaviour
+
+Two callbacks are provided by default in this library. An `onChange` and an `onSubmit`.
+`onChange` will provide the current input each time a key is pressed.
+`onSubmit` will provide the current input and clear the input after the value is returned. This is triggered by default on the enter key press.
+
+```
+import input from 'global-input';
+
+const options = {}
+
+function onChange(value) {
+    // do something
+}
+
+function onSubmit(value) {
+    // do something
+}
+
+const globalInput = new GlobalInput(onChange, onSubmit, options)
+```
+
+### Debug
+You can see when the input is mounted, unmounted, and all key events.
+```
+const options {
+    debug: true
+}
+```
+
+### Submit Key
+You can specify the key that will submit the input
+```
+const options {
+    submitKey: 'Tab'
+}
+```
+
+### Excluding Focused Elements
+You can disable the global input when other elements are in focus. You can pass in elements or node names.
+```
+const options {
+    excludeNodes: [el1, el2],
+    excludeNodeNames: ['INPUT']
+}
+```
+
+### Unmounting and Remounting an Input
+
+You can stop/start the global listener and halt the input in it's current state.
+```
+globalInput.unmount();
+globalInput.mount();
+```
---
+++ index.js
@@ -0,0 +1,82 @@
+class GlobalInput {
+    constructor(onChange, onSubmit, options = {}){
+        if (!document) throw Error('`document` not found')
+
+        this.options = Object.assign({
+            submitKey: 'Enter',
+            debug: false,
+            excludeNodes: [],
+            excludeNodeNames: []
+        }, options)
+        this.onChange = onChange
+        this.onSubmit = onSubmit
+        this.inputCache = ''
+
+        this.mount = this.mount.bind(this)
+        this.unmount = this.unmount.bind(this)
+        this.action = this.action.bind(this)
+
+        this.mount()
+    }
+
+    handle(method, value) {
+        if (this.options.debug) {
+            console.info('Method: ' + method)
+            console.info('Input Value: ' + this.inputCache)
+            console.log('')
+        }
+
+        this['on' + method](value)
+    }
+
+    mount() {
+        document.body.addEventListener('keydown', this.action)
+        if (this.options.debug) console.warn('mounted')
+    }
+
+    unmount() {
+        document.body.removeEventListener('keydown', this.action)
+        if (this.options.debug) console.warn('unmounted')
+    }
+
+    action(e) {
+        if (
+            this.options.excludeNodes.includes(document.activeElement) ||
+            this.options.excludeNodeNames.includes(document.activeElement.nodeName)
+        ) {
+            return
+        }
+
+        switch(e.key) {
+            case this.options.submitKey:
+                this.handle('Submit', this.inputCache)
+
+                this.inputCache = ''
+                this.handle('Change', this.inputCache)
+
+                return
+                break
+
+            case 'Backspace':
+                this.inputCache = this.inputCache.slice(0, -1)
+
+                break
+
+            default:
+                const caps = e.getModifierState && (
+                    e.getModifierState('CapsLock') ||
+                    e.getModifierState('Shift')
+                )
+
+                this.inputCache += (
+                    e.key.length === 1 ? e.key : ''
+                )[caps ? 'toUpperCase' : 'toLowerCase']()
+
+                break
+        }
+
+        this.handle('Change', this.inputCache)
+    }
+}
+
+module.exports = GlobalInput
