4c124fe1cdcf9dd0dbf1b52e118f44d9e08d1f40
Dan Winter-Wijntjes <danwinter1@me.com>
2018-02-01 17:27:33 +1100

    Revert "Add babel"
    This reverts commit 9358de0d77a6198cd2d18d944fd432191a18722c.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,9 @@
-const webpack = require("webpack");
 const path = require('path');
 
 module.exports = {
-    entry: ['babel-polyfill', './index.js'],
+    entry: './index.js',
     output: {
         filename: './bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: [
-                    path.resolve(__dirname, "./index.js"),
-                ]
-            }
-        ]
     }
 };
