9358de0d77a6198cd2d18d944fd432191a18722c
Dan Winter-Wijntjes <danwinter1@me.com>
2018-02-01 14:48:12 +1100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+const webpack = require("webpack");
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: ['babel-polyfill', './index.js'],
     output: {
         filename: './bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [
+                    path.resolve(__dirname, "./index.js"),
+                ]
+            }
+        ]
     }
 };
